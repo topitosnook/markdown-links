6579720d99d6618e7c098ff93e2f6057314423f7
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-08-23 19:07:33 -0400

    add functions to read a md file and get lines with links

--- read.js
+++ read.js
@@ -1,1 +1,33 @@
-const route = '';
+const fs = require("fs");
+
+const route = "./testing.md";
+
+const read = () => {
+  try {
+    return fs.readFileSync(route, "utf8");
+  } catch (err) {
+    return err;
+  }
+};
+
+// console.log(read());
+
+// With lineReader library (npm install line-reader) and showdown converter (npm install showdown)
+const lineReader = require("line-reader");
+const { listeners } = require('process');
+const showdown = require("showdown");
+const converter = new showdown.Converter();
+const html = converter.makeHtml(read()); 
+const lines  = html.split('\n');
+let links = [];
+let counter = 0;
+lines.forEach(line => {
+  // console.log(element);
+  if(line.includes('href')){
+    // console.log(line);
+    links[counter] = line;
+    counter ++;
+  }
+});
+console.log(links);
+
