17cdf7bcf9cd0fe5057e9d4e4e1e82f89c78390c
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-08-23 17:56:03 -0400

    add read functions

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ const fs = require('fs');
 const path = require('path');
 
 const route = '../SCL017-md-link/';
+
+const isThisDirectory = (path) => {
+  // Function that checks if the path is a directory
+  try {
+    const stat = fs.lstatSync(path);
+    return stat.isDirectory();
+  } catch (err) {
+    return false;
+  }
+}
+
+const isThisFile = (path) => {
+  // Function that checks that the path is a file
+  try {
+    const stat = fs.lstatSync(path);
+    return stat.isFile();
+  } catch (err) {
+    return false;
+  }
+}
+
+const readDirectory = (path) => {
+  
+
+}
