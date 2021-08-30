ef2458864ccd9f9a6eccca007cf0a7789686894f
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-08-30 12:55:20 -0400

    add some testing in file

--- test/mdFileCheckFunction.test.js
+++ test/mdFileCheckFunction.test.js
@@ -1,10 +1,15 @@
-const mdLinks = require('../');
+/* eslint-disable */ 
+const checkRoute = require("../mdFileCheckFunction");
 
+const route0 = "../SCL017-md-link/";
+const route1 = "../SCL017-md-lik/";
+const route2 = "testing.md";
+const route3 = "read.js";
 
-describe('mdLinks', () => {
+test("properly reads bad route", () => {
+  expect(checkRoute(route1)).toBe('La ruta no existe');
+});
 
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
+test("properly reads file not md", () => {
+  expect(checkRoute(route3)).toBe('Archivo no es .md');
 });
